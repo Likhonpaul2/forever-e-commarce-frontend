252984bd1ec149f2730f09582eba6fa05a05c3a1
Likhonpaul2 <likhonpaul2@gmail.com>
2025-11-02 01:01:29 +0600

    complete our policy section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       {/* logo   */}
-      <img src={assets.logo} alt="logo" className="w-36" />
+      <Link to={"/"}><img src={assets.logo} alt="logo" className="w-36" /></Link>
 
       {/* navlinks  */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
