09744fb743a4ad6d2ad05b0d316da2b319c3f6b8
Likhonpaul2 <likhonpaul2@gmail.com>
2025-11-02 00:27:43 +0600

    Complete latest collections section

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,12 +1,18 @@
 import { createContext } from "react";
+import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
  const ShopContextProvider = (props)=>{
 
+    const currency = "৳";
+    const delivery_fee = 10;
+    
 
     const value ={
-
+        products,
+        currency,
+        delivery_fee,
     }
     return (
         <ShopContext.Provider value={value}>
